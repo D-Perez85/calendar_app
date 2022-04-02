665aa30085e6fbae911c9fe27060c2d9369cf0f7
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-02 13:33:05 -0300

    Config initial of Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
+import { store } from "./store/store";
+
 
 function App() {
   return (
     <div>
+      <Provider store={store}>
     <AppRouter/>
+
+      </Provider>
     </div>
   );
 }
