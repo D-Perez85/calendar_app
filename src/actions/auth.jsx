b59ce9432a45d6bdb88c21e263e3207b54b565cb
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-04-10 17:00:17 -0300

    Logout of User - clear LocalStorage

--- src/actions/auth.jsx
+++ src/actions/auth.jsx
@@ -74,3 +74,11 @@ const login = ( user ) => ({
     payload: user
 });
 
+export const startLogout = () => {
+    return ( dispatch ) => {
+        localStorage.clear();
+        dispatch( logout() );
+    }
+}
+
+const logout = () => ({ type: types.authLogout })
